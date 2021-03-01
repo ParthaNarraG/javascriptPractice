dc969b51db4e86532657b4354b239b71916f2cd3
Partha <partha.narra123@gmail.com>
2021-03-01 14:27:01 +0530

    new Changes in arraymiddlesort

--- arraymiddlesort.js
+++ arraymiddlesort.js
@@ -2,7 +2,7 @@ let a=[1,2,4,9,10,67,34,45,39,28,3,4,5,6,7,8,1,2,34]
 let b=[]
 a.map((ele,index)=>{
   if(index%3 ===0  &&  index!==0 && index%2!==0){
-    if(index % 3===undefined && index+1===undefined && index+2===undefined){
+    if(index % 3===undefined || index+1===undefined || index+2===undefined){
       console.log(a)
     }
     else{
