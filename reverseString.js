2b8bdf5d7a67733cc5f7a18fff9ac3bf2e1e1459
Partha <partha.narra123@gmail.com>
2021-03-02 23:32:55 +0530

    reverseString updated

--- reverseString.js
+++ reverseString.js
@@ -6,3 +6,13 @@
  str=str.split("").reverse().join("");   //TO REVERSE A STRING CONVERT IT TO ARRAY AND USE REVERSE METHOD AND JOIN IT AS STRING
 
  console.log(str);
+
+ //WITHOUT USING AN REVERSE FUNCTION
+
+let rest="Partha Narra";
+rest=rest.split("");
+let string=""
+for(let i=rest.length-1;i>=0;i--){
+    string+=rest.pop() 
+}
+console.log(string);
