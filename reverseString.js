cb4737c3e45163ed0e3e987dfb893613af0f443c
Partha <partha.narra123@gmail.com>
2021-03-02 22:52:40 +0530

    reverseString added

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+/**
+ * REVERSE A STRING 
+ */
+
+ let str="Partha Narra";
+ str=str.split("").reverse().join("");   //TO REVERSE A STRING CONVERT IT TO ARRAY AND USE REVERSE METHOD AND JOIN IT AS STRING
+
+ console.log(str);
