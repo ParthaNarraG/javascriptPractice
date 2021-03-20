aa081ab02d670539f99d46b9dc7c0d6ef426871c
Partha <partha.narra123@gmail.com>
2021-03-20 21:27:26 +0530

    object methods were added

--- objectMethod.js
+++ objectMethod.js
@@ -4,10 +4,10 @@ const obj={
   }
   
 //remove key from an object using delete operator
-
 delete obj.officeLevelId;
 
 //To find how many keys does an object contains
+Object.keys(obj).length
 
-Object.keys(obj.length)
-
+//Freeze Method stops all the operations for that object , you can't update or create or delete a key
+Object.freeze(obj)
