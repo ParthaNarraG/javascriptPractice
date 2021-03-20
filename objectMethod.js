b729b59165ca88a64e48a3fc8d454ad2dcfe6b05
Partha <partha.narra123@gmail.com>
2021-03-20 14:32:40 +0530

    added object methods

---
+++ objectMethod.js
@@ -0,0 +1,13 @@
+const obj={
+    appId:'1',
+    officeLevelId:"1",
+  }
+  
+//remove key from an object using delete operator
+
+delete obj.officeLevelId;
+
+//To find how many keys does an object contains
+
+Object.keys(obj.length)
+
